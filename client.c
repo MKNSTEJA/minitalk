bc2d0160524b66f8ad8a6903944f25236760abb2
Kumara Teja Mummadisetty <kmummadi@2-E-1.42heilbronn.de>
2024-12-07 22:19:52 +0100

    handles multiple strings

--- client.c
+++ client.c
@@ -6,7 +6,7 @@
 /*   By: kmummadi <kmummadi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/17 12:37:49 by kmummadi          #+#    #+#             */
-/*   Updated: 2024/11/17 16:45:42 by kmummadi         ###   ########.fr       */
+/*   Updated: 2024/12/07 21:52:17 by kmummadi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,18 +18,22 @@ int	main(int argc, char **argv)
 {
 	pid_t	pid;
 	char	*msg;
+	int		i;
 
+	i = 2;
 	pid = 0;
 	msg = NULL;
-	if (argc != 3)
-	{
+	if (argc < 3)
 		return (-1);
-	}
 	else
 	{
-		msg = argv[2];
-		pid = ft_atoi(argv[1]);
-		transfer_message(pid, msg);
+		while (argv[i])
+		{
+			msg = argv[i];
+			pid = ft_atoi(argv[1]);
+			transfer_message(pid, msg);
+			i++;
+		}
 	}
 	return (0);
 }
@@ -43,7 +47,7 @@ void	transfer_message(pid_t pid, char *msg)
 	j = 0;
 	while (msg[i])
 	{
-        j = 0;
+		j = 0;
 		while (j < 7)
 		{
 			if ((msg[i] >> j) & 1)
@@ -51,7 +55,7 @@ void	transfer_message(pid_t pid, char *msg)
 			else
 				kill(pid, SIGUSR1);
 			j++;
-            usleep(300);
+			usleep(180);
 		}
 		i++;
 	}
